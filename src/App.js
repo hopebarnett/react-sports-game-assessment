4bde73a10655784f6b1bb7b4f0edbc3d5a6e0eae
Hope-Barnett <hopekarola2009@live.com>
2020-08-11 16:57:10 -0400

    commited audio and images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from '../src/components/game/Game'
+import SheridanSquirrelsLogo from './assets/images/squirrel.png';
+import RussiavilleRaccoonsLogo from './assets/images/raccoon.png';
+import BurlingtonBunniesLogo from './assets/images/bunny.png';
+import HammondHoundsLogo from './assets/images/hound.png';
 
-function App() {
+
+
+function App(props) {
+  const raccoons = {
+    name: 'Russiaville Raccoons',
+    logoSrc: RussiavilleRaccoonsLogo
+  }
+
+  const squirrels = {
+    name: 'Sheridan Squirrels',
+    logoSrc: SheridanSquirrelsLogo
+  }
+
+  const bunnies = {
+    name: 'Burlington Bunnies',
+    logoSrc: BurlingtonBunniesLogo
+  }
+
+  const hounds = {
+    name: 'Hammond Hounds',
+    logoSrc: HammondHoundsLogo
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game
+        venue="Sheridan Arena"
+        homeTeam={bunnies}
+        visitingTeam={hounds}
+      />
     </div>
-  );
+  )
 }
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
